018a559b02e812d98218709a5f68cb3af8cd94b1
ppcsdn <55426535+ppcsdn@users.noreply.github.com>
2019-11-05 19:08:08 +0800

    Test6

---
+++ src/main/java/com/example/test/TestGit.java
@@ -0,0 +1,11 @@
+package com.example.test;
+
+/**
+ * @author Pstart
+ * @create 2019-11-05 18:35
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("随意a");
+    }
+}
