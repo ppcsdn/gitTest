8f099e4110884461cd186c69eb7b2b8d21105257
ppcsdn <55426535+ppcsdn@users.noreply.github.com>
2019-11-06 08:38:54 +0800

    Update TestGit.java

--- src/main/java/com/example/test/TestGit.java
+++ src/main/java/com/example/test/TestGit.java
@@ -8,6 +8,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("代码原始状态");
         System.out.println("大佬完成项目任务了");
-        system.out.println("我在dev分支里完成开发任务了")
+        system.out.println("我在dev分支里完成开发任务了");
+System.out.println("大佬完成项目任务了");
     }
 }
