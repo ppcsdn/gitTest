c91fb71b26b796ff29f95b9d492cc2ba3be18bd4
Roy_Peng <Roy_Peng@sz.ctil.com>
2020-01-17 17:20:35 +0800

    测试冲突3

--- src/main/java/com/example/test/Test2.java
+++ src/main/java/com/example/test/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("args2 = " + args);
         System.out.println("args3 = " + args);
-        System.out.println("args4 = " + args);
+        System.out.println("args5 = " + args);
 
     }
 }
