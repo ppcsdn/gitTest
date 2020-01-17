8379c7b91826062fd238bd59f0d6bf6c03841c72
Roy_Peng <Roy_Peng@sz.ctil.com>
2020-01-17 17:11:44 +0800

    测试冲突

--- src/main/java/com/example/test/Test2.java
+++ src/main/java/com/example/test/Test2.java
@@ -1,4 +1,9 @@
 package com.example.test;
 
 public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("args1 = " + args);
+        System.out.println("args2 = " + args);
+        System.out.println("args3 = " + args);
+    }
 }
