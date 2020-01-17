a649f89326e31bb9d356da963ac4d9da37744ff4
Roy_Peng <Roy_Peng@sz.ctil.com>
2020-01-17 17:38:12 +0800

    测试

---
+++ src/main/java/com/example/test/Test3.java
@@ -0,0 +1,8 @@
+package com.example.test;
+
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("args1 = " + args);
+        System.out.println("args2 = " + args);
+    }
+}
