1b34ea302c97404aa6013d7471aa30959f5c0d66
Roy_Peng <Roy_Peng@sz.ctil.com>
2020-01-17 17:48:00 +0800

    测试

--- src/main/java/com/example/test/Test2.java
+++ src/main/java/com/example/test/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("args1 = " + args);
         System.out.println("args2 = " + args);
-        System.out.println("args3 = " + args);
+        System.out.println("args4 = " + args);
 
     }
 }
