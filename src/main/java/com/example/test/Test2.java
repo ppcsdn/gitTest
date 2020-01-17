ba1bf75e3b61a1ed7acf999f96ae14948868ab5d
ppcsdn <55426535+ppcsdn@users.noreply.github.com>
2020-01-17 17:10:49 +0800

    Update Test2.java

--- src/main/java/com/example/test/Test2.java
+++ src/main/java/com/example/test/Test2.java
@@ -3,6 +3,7 @@ package com.example.test;
 public class Test2 {
   
    public static void main(String[] args) {
+     System.out.println("args0 = " + args);
         System.out.println("args1 = " + args);
         System.out.println("args2 = " + args);
     }
