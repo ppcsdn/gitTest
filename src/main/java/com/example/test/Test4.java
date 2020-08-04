f9ee367581f3d64bcb654d590cfa167b51d3e32c
ppcsdn <55426535+ppcsdn@users.noreply.github.com>
2020-08-04 12:34:00 +0800

    Update Test4.java

--- src/main/java/com/example/test/Test4.java
+++ src/main/java/com/example/test/Test4.java
@@ -8,4 +8,8 @@ public class Test4 {
     public static void main(String[] args) {
         System.out.printf("");
     }
+   void test4(){
+        int i = 0; int b = 0;
+        System.out.println();
+    }
 }
